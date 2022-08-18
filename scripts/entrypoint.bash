c2639797dfe820091a3c473702452a28b1a22dfb
Elliot Schot <Elliot.Schot@cultureamp.com>
2022-08-18 23:10:05 +1000

    add comment to entrypoint

--- scripts/entrypoint.bash
+++ scripts/entrypoint.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# use the enivronment override for the user id and group id, otherwise use the default backup id of 34
+# optional override for the backup user id
 if [[ ! -z "${BUID}" ]]; then
 	echo "INIT: Updating backup user id to $BUID..."
 	usermod -o -u "$BUID" backup
 fi
-
+# optional override for the backup group id
 if [[ ! -z "${BGID}" ]]; then
 	echo "INIT: Updating backup group id to $BGID..."
 	groupmod -o -g "$BGID" backup
 fi
-
+# start
 runsvdir /runit
